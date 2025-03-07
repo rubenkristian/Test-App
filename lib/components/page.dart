dbe61cdd1bfcb66836394e3c0fd7a2d96b4227aa
Kristian Ruben Sianturi <rubenkristian96@gmail.com>
2025-03-07 23:38:13 +0700

    adjustment: remove dependency
    - remove dependency
    - use pageview

--- lib/components/page.dart
+++ lib/components/page.dart
@@ -7,29 +7,79 @@ class PageSlide extends StatelessWidget {
   const PageSlide({super.key, required this.description, required this.title});
 
   @override
-  Widget build(Object context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          SizedBox(height: 480),
-          Text(
-            title,
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.justify,
-          ),
-          SizedBox(height: 10),
-          Text(
-            description,
-            style: TextStyle(color: Colors.black, fontSize: 14),
-            textAlign: TextAlign.justify,
-          ),
-        ],
-      ),
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        bool isLandscape = constraints.maxWidth > constraints.maxHeight;
+
+        return Container(
+          padding: const EdgeInsets.all(10),
+          child:
+              isLandscape
+                  ? Row(
+                    children: [
+                      /// Image on the Left
+                      Expanded(
+                        flex: 4,
+                        child: Image.asset('assets/logo.png', height: 250),
+                      ),
+
+                      /// Spacing between Image and Text
+                      SizedBox(width: 20),
+
+                      /// Text on the Right
+                      Expanded(
+                        flex: 6,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              title,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              description,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                  : Column(
+                    children: [
+                      Image.asset('assets/logo.png', height: 400),
+                      SizedBox(height: 30),
+                      Text(
+                        title,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        description,
+                        style: TextStyle(color: Colors.black, fontSize: 14),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+        );
+      },
     );
   }
 }
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,10 +1,27 @@
 import 'package:app/components/page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
 import 'package:go_router/go_router.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  // ignore: no_logic_in_create_state
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  int pagePosition = 0;
+  int length = 3;
+
+  List<String> listButtonLabel = <String>["Get Started", "Next Step", "Finish"];
+
+  final PageController pageController = PageController(
+    initialPage: 0,
+    viewportFraction: 1,
+  );
 
   final String description =
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
@@ -12,36 +29,85 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: OnBoardingSlider(
-        totalPage: 3,
-        headerBackgroundColor: Colors.white,
-        background: [
-          Image.asset('assets/logo.png', height: 300),
-          Image.asset('assets/logo.png', height: 300),
-          Image.asset('assets/logo.png', height: 300),
-        ],
-        pageBodies: [
-          PageSlide(
-            description: description,
-            title: "Let's meet our summer coffee drinks",
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                flex: 8,
+                child: PageView.builder(
+                  controller: pageController,
+                  itemBuilder: (BuildContext context, int position) {
+                    return PageSlide(
+                      description: description,
+                      title: "Let's meet our summer coffee drinks",
+                    );
+                  },
+                  itemCount: length,
+                  onPageChanged: (value) {
+                    setState(() {
+                      pagePosition = value;
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 2),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(length, (int index) {
+                    return AnimatedContainer(
+                      duration: Duration(milliseconds: 300),
+                      margin: EdgeInsets.symmetric(horizontal: 4),
+                      width: pagePosition == index ? 20 : 8,
+                      height: 8,
+                      decoration: BoxDecoration(
+                        color:
+                            pagePosition == index ? Colors.black : Colors.grey,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    );
+                  }),
+                ),
+              ),
+
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.green),
+
+                  onPressed: () {
+                    if (pagePosition == length - 1) {
+                      context.go('/detail');
+                      return;
+                    }
+                    pageController.nextPage(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.linear,
+                    );
+                  },
+                  child: Text(
+                    listButtonLabel[pagePosition],
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
           ),
-          PageSlide(
-            description: description,
-            title: "Let's meet our summer coffee drinks",
-          ),
-          PageSlide(
-            description: description,
-            title: "Let's meet our summer coffee drinks",
-          ),
-        ],
-        speed: 1.0,
-        onFinish: () {
-          context.go('/detail');
-        },
-        controllerColor: Colors.green,
-        finishButtonText: "Get Started",
-        finishButtonStyle: FinishButtonStyle(backgroundColor: Colors.green),
+        ),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
   }
 }
